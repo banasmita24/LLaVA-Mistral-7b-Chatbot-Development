004330a7847ebf85cbec2851a00a8b69889164b2
Banasmita Jena <155791058+banasmita24@users.noreply.github.com>
2024-10-18 20:23:28 +0530

    only files

---
+++ html_templates.py
@@ -0,0 +1,21 @@
+css = """
+    <style>
+        /* User Chat Message */
+
+        .st-emotion-cache-janbn0 {
+            background-color: #2b313e;
+            text-color: black;
+        }
+
+        /* AI Chat Message */
+
+        .st-emotion-cache-4oy321 {
+            background-color: #b3c8f2;
+            text-color: black;
+        }
+
+        section[data-testid="stSidebar"] {
+            width: 380px !important;
+        }
+    </style>
+    """
